778000731c13bc47c638a31e048a2d9ff069fae7
helloaltop <helloaltop@gmail.com>
2021-08-19 11:05:29 +0700

    First Commit

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xff314728);
+Color background = Color(0xffFFFFFF);
+Color orange = Color(0xffF9B650);
+Color green = Color(0xff81CB67);
