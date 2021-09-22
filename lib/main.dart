2f2e173de10eba4862ab918505e1dd8774407eae
helloaltop <helloaltop@gmail.com>
2021-09-22 13:40:27 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,11 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Basse'),
-          ),
-          body: Image.asset('assets/images/primary.png')),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,1 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Profile Picture',
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.asset(
+                'assets/images/primary.png',
+                width: 140,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Muhammad Ali Topan',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
